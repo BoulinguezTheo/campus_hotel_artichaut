c47eb8fa257e49ad4591ce666cccc079d43e304a
Efkan Ipek <efkan.ipek@le-campus-numerique.fr>
2023-10-23 11:18:20 +0200

    project structure

---
+++ src/test/java/com/campus/campus_hotel_artichaut_backend/CampusHotelArtichautBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.campus_hotel_artichaut_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHotelArtichautBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
