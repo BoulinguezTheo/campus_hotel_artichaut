f1cd48973ccf106e76ea87a3212b8d2b89fc37e2
azarual01 <elghazi.azarual@le-campus-numerique.fr>
2023-10-24 16:52:40 +0200

    authentication part1

---
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/payload/response/AuthenticationResponse.java
@@ -0,0 +1,29 @@
+package com.campus.campus_hotel_artichaut_backend.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.campus.campus_hotel_artichaut_backend.enums.TokenType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private Long id;
+    private String email;
+    private List<String> roles;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+
+}
