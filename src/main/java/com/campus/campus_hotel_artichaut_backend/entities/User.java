ba52a2cc92bfd129544f085f1d3d24bc4660f6f0
azarual01 <elghazi.azarual@le-campus-numerique.fr>
2023-10-24 13:22:37 +0200

    jwt

---
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/entities/User.java
@@ -0,0 +1,41 @@
+package com.campus.campus_hotel_artichaut_backend.entities;
+
+import java.util.List;
+import java.util.Collection;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import com.campus.campus_hotel_artichaut_backend.enums.Role;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+}
---
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/enums/Privilege.java
@@ -0,0 +1,9 @@
+package com.campus.campus_hotel_artichaut_backend.enums;
+
+public enum Privilege {
+    READ_PRIVILEGE,
+    WRITE_PRIVILEGE,
+    DELETE_PRIVILEGE,
+    UPDATE_PRIVILEGE,
+    ;
+}
