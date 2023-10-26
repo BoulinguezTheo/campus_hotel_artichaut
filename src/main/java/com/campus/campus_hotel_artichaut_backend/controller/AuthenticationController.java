a9f6b2c024d02ee086edabfbe240a210f79cdb71
azarual01 <elghazi.azarual@le-campus-numerique.fr>
2023-10-26 14:22:30 +0200

    jwt autentication updated

--- src/main/java/com/campus/campus_hotel_artichaut_backend/controller/AuthenticationController.java
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/controller/AuthenticationController.java
@@ -1,16 +1,13 @@
 package com.campus.campus_hotel_artichaut_backend.controller;
 
-import com.campus.campus_hotel_artichaut_backend.entities.User;
 import com.campus.campus_hotel_artichaut_backend.payload.request.AuthenticationRequest;
 import com.campus.campus_hotel_artichaut_backend.payload.request.RefreshTokenRequest;
 import com.campus.campus_hotel_artichaut_backend.payload.request.RegisterRequest;
 import com.campus.campus_hotel_artichaut_backend.payload.response.AuthenticationResponse;
 import com.campus.campus_hotel_artichaut_backend.payload.response.RefreshTokenResponse;
-
-import com.campus.campus_hotel_artichaut_backend.repository.UserRepository;
 import com.campus.campus_hotel_artichaut_backend.service.AuthenticationService;
-import com.campus.campus_hotel_artichaut_backend.service.RefreshTokenService;
 import com.campus.campus_hotel_artichaut_backend.service.JwtService;
+import com.campus.campus_hotel_artichaut_backend.service.RefreshTokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +15,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 
 
 @RestController
@@ -41,52 +37,53 @@ public class AuthenticationController {
         ResponseCookie refreshTokenCookie = refreshTokenService.generateRefreshTokenCookie(authenticationResponse.getRefreshToken());
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
-                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
                 .body(authenticationResponse);
     }
 
-    @PostMapping("/user")
+    @PostMapping("/tokens")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         AuthenticationResponse authenticationResponse = authenticationService.authenticate(request);
         ResponseCookie jwtCookie = jwtService.generateJwtCookie(authenticationResponse.getAccessToken());
         ResponseCookie refreshTokenCookie = refreshTokenService.generateRefreshTokenCookie(authenticationResponse.getRefreshToken());
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE,jwtCookie.toString())
-                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
                 .body(authenticationResponse);
     }
-    @PostMapping("/refresh-token")
+
+    @PutMapping("/tokens/refresh")
     public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
         return ResponseEntity.ok(refreshTokenService.generateNewToken(request));
     }
 
-    @PostMapping("/refresh-token-cookie")
-    public ResponseEntity<Void> refreshTokenCookie(HttpServletRequest request) {
+    @PostMapping("/tokens/refresh-cookie")
+    public ResponseEntity<RefreshTokenResponse> refreshTokenCookie(HttpServletRequest request) {
         String refreshToken = refreshTokenService.getRefreshTokenFromCookies(request);
         RefreshTokenResponse refreshTokenResponse = refreshTokenService
                 .generateNewToken(new RefreshTokenRequest(refreshToken));
         ResponseCookie NewJwtCookie = jwtService.generateJwtCookie(refreshTokenResponse.getAccessToken());
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, NewJwtCookie.toString())
-                .build();
-    }
-    @GetMapping("/info")
-    public Authentication getAuthentication(@RequestBody AuthenticationRequest request){
-        return     authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+                .body(refreshTokenResponse);
     }
 
-    @DeleteMapping("/user")
-    public ResponseEntity<Void> logout(HttpServletRequest request){
+    @GetMapping("/tokens/current")
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @DeleteMapping("/tokens")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
         String refreshToken = refreshTokenService.getRefreshTokenFromCookies(request);
-        if(refreshToken != null) {
+        if (refreshToken != null) {
             refreshTokenService.deleteByToken(refreshToken);
         }
         ResponseCookie jwtCookie = jwtService.getCleanJwtCookie();
         ResponseCookie refreshTokenCookie = refreshTokenService.getCleanRefreshTokenCookie();
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE,jwtCookie.toString())
-                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
                 .build();
 
     }
