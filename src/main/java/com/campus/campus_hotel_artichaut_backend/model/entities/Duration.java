8e759ce11bd3822469a2ea7402655bc982d5b676
Theo <119578352+BoulinguezTheo@users.noreply.github.com>
2023-11-27 13:30:31 +0100

    fix duplicated files (#22)

--- src/main/java/com/campus/campus_hotel_artichaut_backend/model/entities/Duration.java
+++
@@ -1,8 +0,0 @@
-package com.campus.campus_hotel_artichaut_backend.model.entities;
-
-public enum Duration {
-    ONE_TIME,
-    DAILY,
-    WEEKLY,
-    ONCE,
-}
--- src/main/java/com/campus/campus_hotel_artichaut_backend/model/entities/Reference.java
+++
@@ -1,8 +0,0 @@
-package com.campus.campus_hotel_artichaut_backend.model.entities;
-
-public enum Reference {
-
-    // TODO : what is this
-    OPTION,
-    ROOM
-}
