2aee91271058f92f237ffba9c799668a6eb3664f
efkanipek <efkan.ipek@le-campus-numerique.fr>
2023-11-30 10:42:57 +0100

    use custom exception for no room available

--- src/main/java/com/campus/campus_hotel_artichaut_backend/controller/ReservationController.java
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/controller/ReservationController.java
@@ -1,12 +1,15 @@
 package com.campus.campus_hotel_artichaut_backend.controller;
 
 import com.campus.campus_hotel_artichaut_backend.dto.ReservationDto;
+import com.campus.campus_hotel_artichaut_backend.exception.NoRoomAvailableException;
 import com.campus.campus_hotel_artichaut_backend.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +20,10 @@ public class ReservationController {
 
     @PostMapping
     public ReservationDto reserveRoom(@RequestBody ReservationDto reservationDto) {
-        return this.reservationService.reserveRoom(reservationDto);
+        try {
+            return this.reservationService.reserveRoom(reservationDto);
+        } catch (NoRoomAvailableException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
