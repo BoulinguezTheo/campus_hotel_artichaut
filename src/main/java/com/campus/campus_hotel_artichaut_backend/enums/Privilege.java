bb0bb013b2586b03ef170a76edabd56754889255
azarual01 <elghazi.azarual@le-campus-numerique.fr>
2023-11-30 11:09:24 +0100

    conflicts resolved

--- src/main/java/com/campus/campus_hotel_artichaut_backend/enums/Privilege.java
+++
@@ -1,9 +0,0 @@
-package com.campus.campus_hotel_artichaut_backend.enums;
-
-public enum Privilege {
-    READ_PRIVILEGE,
-    WRITE_PRIVILEGE,
-    DELETE_PRIVILEGE,
-    UPDATE_PRIVILEGE,
-    ;
-}
