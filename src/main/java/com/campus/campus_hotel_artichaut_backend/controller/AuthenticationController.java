21c5b09d2f4a3bd0a228422ceb0ca96a36202dfd
azarual01 <119579007+azarual01@users.noreply.github.com>
2023-11-30 15:39:41 +0100

    Jwt (#24)
    * jwt
    
    * authentication part1
    
    * authentication part2
    
    * twt completed
    
    * jwt autentication updated
    
    * nothing to merge only for testing
    
    * inscription linking front and back
    
    * conflicts resolved
    
    * correction et adaptation avec le front
    
    * port fixed
    
    * fix rebase conflicts
    
    ---------
    
    Co-authored-by: Theo <119578352+BoulinguezTheo@users.noreply.github.com>

--- src/main/java/com/campus/campus_hotel_artichaut_backend/controller/AuthenticationController.java
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/controller/AuthenticationController.java
@@ -20,6 +20,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/authentication")
 @RequiredArgsConstructor
--- src/main/java/com/campus/campus_hotel_artichaut_backend/security/payload/request/RegisterRequest.java
+++ src/main/java/com/campus/campus_hotel_artichaut_backend/security/payload/request/RegisterRequest.java
@@ -15,18 +15,21 @@ import lombok.Data;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+
     @NotBlank(message = "firstname is required")
     private String firstname;
+
     @NotBlank(message = "lastname is required")
     private String lastname;
+
     @NotBlank(message = "email is required")
     @Email(message = "email format is not valid")
     private String email;
+
     @NotBlank(message = "password is required")
     @StrongPassword
     private String password;
-    @NotNull
-    private Role role;
+
     @NotBlank(message = "address is required")
     private String address;
 }
